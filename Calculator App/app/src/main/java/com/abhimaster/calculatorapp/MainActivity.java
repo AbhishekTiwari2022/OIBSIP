2cbb39b455e0b2d27b186355ac8906a879901f17
Abhishek Tiwari <tpradum97@gmail.com>
2023-05-24 14:40:57 +0530

    Task3

--- Calculator App/app/src/main/java/com/abhimaster/calculatorapp/MainActivity.java
+++ Calculator App/app/src/main/java/com/abhimaster/calculatorapp/MainActivity.java
@@ -8,7 +8,8 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    /* Task:3
+    * Calculator App BY Abhishek Tiwari */
     String oldNumber;
     String op = "+";
     EditText edtext;
